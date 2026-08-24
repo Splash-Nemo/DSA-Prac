8cd40ae2319a71f3c398fb3338f8df5f37f58583
Splash-Nemo <ap457codes@gmail.com>
2026-08-24 19:13:49 +0530

    --allow-empty-message

---
+++ JobSequencing.java
@@ -0,0 +1,43 @@
+class Solution {
+    public ArrayList<Integer> jobSequencing(int[] d, int[] p) {
+        // code here
+        
+        int maxD= 0;
+        for(int i:d) maxD= Math.max(i,maxD);
+        
+        int count= 0, maxProfit= 0;
+        
+        List<int[]> rec= new ArrayList<>();
+        int n= d.length;
+        
+        for(int i=0; i<n; i++) rec.add(new int[]{d[i], p[i]});
+        
+        rec.sort(Comparator.comparingInt((int[] a) -> a[1]).reversed());
+        
+        int[] slots= new int[maxD+1];
+        
+        for(int i=0; i<rec.size(); i++){
+            int deadline= rec.get(i)[0];
+            int profit= rec.get(i)[1];
+            
+            int j=deadline;
+            while(j>0){
+                if(slots[j]==0){
+                    slots[j]=1;
+                    count+=1;
+                    maxProfit+=profit;
+                    break;
+                }
+                
+                j-=1;
+            }
+        }
+        
+        
+        ArrayList<Integer> ans= new ArrayList<>();
+        ans.add(count);
+        ans.add(maxProfit);
+        
+        return ans;
+    }
+}
---
+++ JobSequencingProblem.java
@@ -0,0 +1,40 @@
+class Solution {
+    public ArrayList<Integer> jobSequencing(int[] d, int[] p) {
+        
+         int n = d.length;
+         List<int[]> rec = new ArrayList<>();
+         int maxD = 0;
+
+         for (int i = 0; i < n; i++) {
+             rec.add(new int[]{d[i], p[i]});
+             maxD = Math.max(maxD, d[i]);
+         }
+
+         rec.sort((a, b) -> Integer.compare(b[0], a[0]));
+
+         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+         int count = 0;
+         int maxProfit = 0;
+         int j = 0;
+
+         for (int slot = maxD; slot > 0; slot--) {
+
+             while (j < n && rec.get(j)[0] >= slot) {
+                 maxHeap.add(rec.get(j)[1]);
+                 j++;
+             }
+
+             if (!maxHeap.isEmpty()) {
+                 maxProfit += maxHeap.poll();
+                 count++;
+             }
+         }
+
+         ArrayList<Integer> ans = new ArrayList<>();
+         ans.add(count);
+         ans.add(maxProfit);
+         
+         return ans;
+     }
+ }
